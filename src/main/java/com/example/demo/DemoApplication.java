e4ad8257daa369594e2b44c9fe2eca7e1b49db31
Vaishnavia <52031729+vaishnavia4@users.noreply.github.com>
2024-03-23 02:30:47 -0400

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.DemoApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
